42261029a784abfe36bc4db354dc16bc7d12e7d6
Wesley Rodrigues da Silva <wesley.it@gmail.com>
2015-12-13 15:43:20 -0200

    Changed to use a single dotemacs.el file.

--- init.el
+++ init.el
@@ -7,24 +7,7 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
-(setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
-(let* ((org-dir (expand-file-name
-                 "lisp" (expand-file-name
-                         "org" (expand-file-name
-                                "src" dotfiles-dir))))
-       (org-contrib-dir (expand-file-name
-                         "lisp" (expand-file-name
-                                 "contrib" (expand-file-name
-                                            ".." org-dir))))
-       (load-path (append (list org-dir org-contrib-dir)
-                          (or load-path nil))))
-
-	;; load up Org-mode and Org-babel
-  (require 'org-install)
-  (require 'ob-tangle))
-
-;; load up all literate org-mode files in this directory
-(mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
+(load-file "~/.emacs.d/org-dotemacs.el")
+(org-dotemacs-load-file "~/.emacs.d/my_emacs_config.org" "")
 
 ;;; init.el ends here
