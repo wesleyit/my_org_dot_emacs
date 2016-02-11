b72f8f90ef772497c64f934a4d4bab8a7cb7ba70
Wesley Rodrigues da Silva <wesley.it@gmail.com>
2016-02-11 08:10:57 -0200

    Added projectile-bookmarks.eld to gitignore

--- custom.el
+++ custom.el
@@ -7,7 +7,11 @@
    (quote
     ("b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "0fb6369323495c40b31820ec59167ac4c40773c3b952c264dd8651a3b704f6b5" "3632cf223c62cb7da121be0ed641a2243f7ec0130178722554e613c9ab3131de" "66132890ee1f884b4f8e901f0c61c5ed078809626a547dbefbb201f900d03fd8" "f04122bbc305a202967fa1838e20ff741455307c2ae80a26035fbf5d637e325f" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
- '(safe-local-variable-values (quote ((docker-image-name . "your-image-name-here")))))
+ '(safe-local-variable-values
+   (quote
+    ((docker-image-name . "wesleyit/ws-riak:2.1.3")
+     (docker-image-name . "wesleyit/ws-riak:2.1")
+     (docker-image-name . "your-image-name-here")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
